b13a3e32834b13eb77039482b3c4f6acbdfee2ea
Hao Chen <hao.chen@yale.edu>
2022-10-03 16:28:04 -0400

    update replay with timing

--- test/mavlink_msg_replay.py
+++ test/mavlink_msg_replay.py
@@ -8,6 +8,8 @@ import time
 import socket
 import pickle
 import atexit
+import threading
+import tqdm
 from pprint import pprint
 from pymavlink.dialects.v20 import ardupilotmega as mavlink2
 
@@ -90,7 +92,7 @@ class UdpAdapter(Adapter):
 
 
 class Loader:
-    Adapters : dict = {
+    Adapters: dict = {
         'stdio': StdioAdapter,
         'pipe': PipeAdapter,
         'tcp': TcpAdapter,
@@ -107,17 +109,49 @@ class Loader:
         return self.msg_list
 
 
+class Replayer(threading.Thread):
+    progress = None
+
+    def __init__(self, adapter: Adapter, msg_list: list):
+        super().__init__()
+        if Replayer.progress is None:
+            Replayer.progress = tqdm.tqdm(total=len(msg_list))
+        self.adapter = adapter
+        self.msg_list = msg_list
+
+    def run(self):
+        last = 0
+        duration = 0
+        for ms, msg in self.msg_list:
+            time.sleep(max(0, ms - last - duration) / 1000)
+            last = ms
+            start = time.time()
+            self.adapter.send(msg)
+            end = time.time()
+            duration = int((end - start) * 1000)
+            Replayer.progress.update(1)
+
+
 class Main:
     @staticmethod
     def main():
-        argparse.ArgumentParser(description='Replay mavlink messages from a file')
+        argparse.ArgumentParser(
+            description='Replay mavlink messages from a file')
         parser = argparse.ArgumentParser()
-        parser.add_argument('--load', default='mavmsg_dump.bin', help='Path to the file containing the messages')
-        parser.add_argument('--adapter', nargs='+', default=['stdio'], choices=Loader.Adapters.keys(), help='Adapter to use')
-        parser.add_argument('--host', default='localhost', help='Host to connect to')
-        parser.add_argument('--tcp', default=12001, type=int, help='Port to connect to')
-        parser.add_argument('--udp', default=12002, type=int, help='Port to connect to')
-        parser.add_argument('--pipe', default=os.path.join(os.getcwd(), 'mavlink_replay_pipe'), help='Path to the pipe')
+        parser.add_argument('--load', default='mavmsg_dump.bin',
+                            help='Path to the file containing the messages')
+        parser.add_argument('--adapter', nargs='+', default=['stdio'],
+                            choices=Loader.Adapters.keys(),
+                            help='Adapter to use')
+        parser.add_argument('--host', default='localhost',
+                            help='Host to connect to')
+        parser.add_argument('--tcp', default=12001, type=int,
+                            help='Port to connect to')
+        parser.add_argument('--udp', default=12002, type=int,
+                            help='Port to connect to')
+        parser.add_argument('--pipe', default=os.path.join(os.getcwd(),
+                                                           'mavlink_replay_pipe'),
+                            help='Path to the pipe')
         args = parser.parse_args()
 
         loader = Loader(args.load)
@@ -136,11 +170,13 @@ class Main:
         for adapter in adapters:
             atexit.register(adapter.cleanup)
 
-        for msg in loader.msg_list:
-            for adapter in adapters:
-                adapter.send(msg)
+        threads = []
+        for adapter in adapters:
+            threads.append(Replayer(adapter, loader.msg_list))
+
+        for thread in threads:
+            thread.start()
 
 
 if __name__ == '__main__':
     Main.main()
-
