64a35c071a97a0813ea0c6aedb8d67c1eb6532cc
Man-Ki Yoon <man-ki.yoon@ncsu.edu>
2022-10-04 15:52:55 -0400

    security_policies.c

--- lib/security_policies.c
+++ lib/security_policies.c
@@ -34,6 +34,14 @@ int security_policy_check_accept(
     return true;
 }
 
+int security_policy_reject_mavlink_cmd_meminfo(
+	const struct security_policy_t* policy, const struct message_t* msg, size_t* attribute)
+{
+    if (msg->msg.msgid == MAVLINK_MSG_ID_MEMINFO)
+	   return false;
+    return true;
+}
+
 int security_policy_reject_mavlink_cmd_waypoint(
     const struct security_policy_t* policy, const struct message_t* msg, size_t* attribute)
 {
@@ -72,6 +80,7 @@ enum policy_id_t
     POLICY_ID_ACCEPT_VMC,
     POLICY_ID_REJECT_NAV_WAYPOINT,
     POLICY_ID_REJECT_DISABLE_GEOFENCE,
+    POLICY_ID_REJECT_MEMINFO,
 };
 
 void security_policy_init(struct pipeline_t * pipeline)
@@ -79,5 +88,6 @@ void security_policy_init(struct pipeline_t * pipeline)
     policy_register(&pipeline->policies, POLICY_ID_ACCEPT_VMC, security_policy_match_vmc, security_policy_check_accept);
     policy_register(&pipeline->policies, POLICY_ID_REJECT_NAV_WAYPOINT, security_policy_match_mmc, security_policy_reject_mavlink_cmd_waypoint);
     policy_register(&pipeline->policies, POLICY_ID_REJECT_DISABLE_GEOFENCE, security_policy_match_mmc, security_policy_reject_mavlink_cmd_disable_geofence);
+    policy_register(&pipeline->policies, POLICY_ID_REJECT_MEMINFO, security_policy_match_mmc, security_policy_reject_mavlink_cmd_meminfo);
     /* ... */
 }
