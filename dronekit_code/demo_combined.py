5e1d5f0a32bd80c7a307e6991bad74563dcddb43
Man-Ki Yoon <man-ki.yoon@yale.edu>
2022-11-12 16:04:58 -0500

    demo code modified

--- dronekit_code/demo_combined.py
+++ dronekit_code/demo_combined.py
@@ -2,12 +2,46 @@
 from __future__ import print_function
 import time
 from dronekit import connect, VehicleMode, LocationGlobalRelative, Command
-from util import distance_to_current_waypoint
+from util import upload_mission, distance_to_current_waypoint
 
 connection_string = "udpout:127.0.0.1:14660"   #through thinros_app_udp
 #connection_string = "udp:127.0.0.1:14551"   #direction connection to SITL (no secure gateway)
-#connection_string = "/dev/ttyS0"   #RPI 
+#connection_string = "/dev/ttyS0"   #RPI
 vehicle = connect(connection_string, wait_ready=True, timeout=100, rate=1, heartbeat_timeout=100)
+
+vehicle.parameters['RTL_AUTOLAND']=1
+vehicle.mode = VehicleMode("MANUAL")
+
+#Enable geofence
+print("==============================")
+print("Enabling geo fence")
+vehicle.parameters['FENCE_ENABLE']=1
+print("Fence enabled? ", vehicle.parameters['FENCE_ENABLE'])
+print("==============================")
+
+#Upload mission and takeoff
+upload_mission(vehicle, 'mission.txt')
+vehicle.mode = VehicleMode("AUTO")
+vehicle.armed   = True
+
+print("-----------------------------")
+
+while True:
+    mode = vehicle.mode.name
+    lat = vehicle.location.global_relative_frame.lat
+    lon = vehicle.location.global_relative_frame.lon
+    alt = vehicle.location.global_relative_frame.alt
+    airspeed = vehicle.airspeed
+    fence = vehicle.parameters['FENCE_ENABLE']
+    nextwaypoint=vehicle.commands.next
+    dist_to_next_wp = distance_to_current_waypoint(vehicle)
+    print("[%s] lat=%f, lon=%f, alt=%f, airspeed=%f, fence=%d, next wp=%d (%f m)" % (mode, lat, lon, alt, airspeed, fence, nextwaypoint, dist_to_next_wp))
+    if (nextwaypoint==3 and dist_to_next_wp < 300.0):
+        break
+
+    time.sleep(1)
+
+
 
 #Disable geofence
 print("==============================")
@@ -50,6 +84,7 @@ for cmd in cmds:
 print("After WP4(x,y) = (%f, %f)" % (missionlist[4].x, missionlist[4].y))
 
 print("-----------------------------")
+#for i in range(1,5):
 while True:
     mode = vehicle.mode.name
     lat = vehicle.location.global_relative_frame.lat
@@ -59,9 +94,8 @@ while True:
     fence = vehicle.parameters['FENCE_ENABLE']
     nextwaypoint=vehicle.commands.next
     dist_to_next_wp = distance_to_current_waypoint(vehicle)
-    print("[%s] lat=%f, lon=%f, alt=%f, airspeed=%f, fence=%d, next wp=%d (%f m)" % (mode, lat, lon, alt, airspeed, fence, nextwaypoint, dist_to_next_wp))
+    print("[%s] lat=%f, lon=%f, alt=%f, airspeed=%f, fence=%d, next wp=%s (%f m)" % (mode, lat, lon, alt, airspeed, fence, nextwaypoint, dist_to_next_wp))
     time.sleep(1)
 
 
 vehicle.close()
-
---
+++ dronekit_code/util.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+from __future__ import print_function
+import time
+import math
+from dronekit import connect, VehicleMode, LocationGlobalRelative, Command
+
+def readmission(vehicle, aFileName):
+    """
+    Load a mission from a file into a list.
+
+    This function is used by upload_mission().
+    """
+    print("Reading mission from file: %s\n" % aFileName)
+    cmds = vehicle.commands
+    missionlist=[]
+    with open(aFileName) as f:
+        for i, line in enumerate(f):
+            if i==0:
+                if not line.startswith('QGC WPL 110'):
+                    raise Exception('File is not supported WP version')
+            else:
+                linearray=line.split('\t')
+                ln_index=int(linearray[0])
+                ln_currentwp=int(linearray[1])
+                ln_frame=int(linearray[2])
+                ln_command=int(linearray[3])
+                ln_param1=float(linearray[4])
+                ln_param2=float(linearray[5])
+                ln_param3=float(linearray[6])
+                ln_param4=float(linearray[7])
+                ln_param5=float(linearray[8])
+                ln_param6=float(linearray[9])
+                ln_param7=float(linearray[10])
+                ln_autocontinue=int(linearray[11].strip())
+                cmd = Command( 0, 0, 0, ln_frame, ln_command, ln_currentwp, ln_autocontinue, ln_param1, ln_param2, ln_param3, ln_param4, ln_param5, ln_param6, ln_param7)
+                missionlist.append(cmd)
+    return missionlist
+
+
+def upload_mission(vehicle, aFileName):
+        """
+        Upload a mission from a file.
+        """
+        #Read mission from file
+        missionlist = readmission(vehicle, aFileName)
+
+        print("\nUpload mission from a file: %s" % aFileName)
+        #Clear existing mission from vehicle
+        print(' Clear mission')
+        cmds = vehicle.commands
+        cmds.clear()
+        #Add new mission to vehicle
+        for command in missionlist:
+            cmds.add(command)
+        print(' Upload mission')
+        vehicle.commands.upload()
+
+
+
+def get_location_metres(original_location, dNorth, dEast):
+    """
+    Returns a LocationGlobal object containing the latitude/longitude `dNorth` and `dEast` metres from the
+    specified `original_location`. The returned Location has the same `alt` value
+    as `original_location`.
+
+    The function is useful when you want to move the vehicle around specifying locations relative to
+    the current vehicle position.
+    The algorithm is relatively accurate over small distances (10m within 1km) except close to the poles.
+    For more information see:
+    http://gis.stackexchange.com/questions/2951/algorithm-for-offsetting-a-latitude-longitude-by-some-amount-of-meters
+    """
+    earth_radius=6378137.0 #Radius of "spherical" earth
+    #Coordinate offsets in radians
+    dLat = dNorth/earth_radius
+    dLon = dEast/(earth_radius*math.cos(math.pi*original_location.lat/180))
+
+    #New position in decimal degrees
+    newlat = original_location.lat + (dLat * 180/math.pi)
+    newlon = original_location.lon + (dLon * 180/math.pi)
+    return LocationGlobal(newlat, newlon,original_location.alt)
+
+
+def get_distance_metres(aLocation1, aLocation2):
+    """
+    Returns the ground distance in metres between two LocationGlobal objects.
+
+    This method is an approximation, and will not be accurate over large distances and close to the
+    earth's poles. It comes from the ArduPilot test code:
+    https://github.com/diydrones/ardupilot/blob/master/Tools/autotest/common.py
+    """
+    dlat = aLocation2.lat - aLocation1.lat
+    dlong = aLocation2.lon - aLocation1.lon
+    return math.sqrt((dlat*dlat) + (dlong*dlong)) * 1.113195e5
+
+
+
+def distance_to_current_waypoint(vehicle):
+    """
+    Gets distance in metres to the current waypoint.
+    It returns None for the first waypoint (Home location).
+    """
+    nextwaypoint = vehicle.commands.next
+    if nextwaypoint==0:
+        return 0
+    missionitem=vehicle.commands[nextwaypoint-1] #commands are zero indexed
+    lat = missionitem.x
+    lon = missionitem.y
+    alt = missionitem.z
+    targetWaypointLocation = LocationGlobalRelative(lat,lon,alt)
+    distancetopoint = get_distance_metres(vehicle.location.global_frame, targetWaypointLocation)
+    return distancetopoint
